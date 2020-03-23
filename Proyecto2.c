2f47d341f9d14687a6dd2ad1d0b52d030ff3bd21
Carmen Xiloj <carmen@MacBook-Air-de-Carmen.local>
2020-03-22 22:09:34 -0600

    Otros avances

--- Proyecto2.c
+++ Proyecto2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #define ROW 32
 #define COL 6
 void clearscreen()
@@ -9,9 +10,14 @@ void clearscreen()
 }
 char vuelo[5];
 int asientos[ROW][COL];
-int fila;
+int fila=0;
 char columna[1];
-int A,B,C,D,E,F;
+int A=1;
+int B=2;
+int C=3;
+int D=4;
+int E=5;
+int F=6;
 void menu();
 void ReservarAsientos();
 void AsientosDisponibles();
@@ -63,28 +69,91 @@ void menu(){
         }
         
 
-    } while ( opcion != 4 );
-return;    
+    } while ( opcion != 4 );  
 }
 void ReservarAsientos(){
     clearscreen();
-    A=1;
-    B=2;
-    C=3;
-    D=4;
-    E=5;
-    F=6;
-    char asiento;
     printf("Vuelo %s\n",vuelo);
-    printf("Ingrese asiento a reservar(Fila,Columna):\n");
-    scanf("%i""%s",&fila,columna);
-
-    printf("Asiento reservado exitosamente");
-    return;
+Reservar:
+    printf("Ingrese asiento a reservar(LETRA,DIGITO):");
+    scanf("%s%i",columna,&fila);
+   char salir;
+   printf("Enter some character. Enter $ to exit...\n");
+   while (salir != 'm')
+   { salir=getchar();
+if (fila<= ROW) {
+  if(columna[0]=='A'||columna[0]=='B'||columna[0]=='C'||columna[0]=='D'||columna[0]=='E'||columna[0]=='F'){
+    switch(columna[0]){
+case 'A':
+    if (asientos[fila][A]==0){
+    printf("Asiento %s%i reservado exitosamente!!\n",columna,fila);
+    asientos[fila][A]='X';
+    goto Reservar;
+    }
+    else {
+        printf("Este asiento ya ha sido reservado\nVuelva a intentarlo");
+        goto Reservar;
+    } break;
+case 'B':
+    if (asientos[fila][B]==0){
+    printf("Asiento %s%i reservado exitosamente!!\n",columna,fila);
+    asientos[fila][B]='X';
+    goto Reservar;
+    }
+    else {
+        printf("Este asiento ya ha sido reservado\nVuelva a intentarlo");
+        goto Reservar;
+    } break;
+case 'C':
+    if (asientos[fila][C]==0){
+    printf("Asiento %s%i reservado exitosamente!!\n",columna,fila);
+    asientos[fila][C]='X';
+    goto Reservar;
+    }
+    else {
+        printf("Este asiento ya ha sido reservado\nVuelva a intentarlo");
+        goto Reservar;
+    } break;
+case 'D':
+    if (asientos[fila][D]==0){
+    printf("Asiento %s%i reservado exitosamente!!\n",columna,fila);
+    asientos[fila][A]='X';
+    goto Reservar;
+    }
+    else {
+        printf("Este asiento ya ha sido reservado\nVuelva a intentarlo");
+        goto Reservar;
+    } break;
+case 'E':
+    if (asientos[fila][E]==0){
+    printf("Asiento %s%i reservado exitosamente!!\n",columna,fila);
+    asientos[fila][A]='X';
+    goto Reservar;
+    }
+    else {
+        printf("Este asiento ya ha sido reservado\nVuelva a intentarlo");
+        goto Reservar;
+    } break;
+case 'F':
+    if (asientos[fila][F]==0){
+    printf("Asiento %s%i reservado exitosamente!!\n",columna,fila);
+    asientos[fila][A]='X';
+    goto Reservar;
+    }
+    else {
+        printf("Este asiento ya ha sido reservado\n");
+        goto Reservar;
+    } break;
+}}  else {
+    printf("Este no es un asiento\nVuelva a intentarlo\n");
+    goto Reservar;}
+}else {printf("Este no es un asiento\nVuelva a intentarlo\n");
+    goto Reservar;}}
+    menu();
 }
 void AsientosDisponibles(){
     clearscreen();
-    printf("Vamos a mostrar asientos disponibles:\n");
+    printf("Estos son los asientos disponibles:\n");
     Asientos();
 
     return;
@@ -93,11 +162,12 @@ void AsientosDisponibles(){
 void Resumen(){
     clearscreen();
     printf("Vamos a mostrar resumen\n");
-    return;
 }
 
 void Asientos(){
     int i;
+    int c;
+    asientos[3][5]='X';
     printf("  ||A| |B| |C| |D| |E| |F|\n");
         
         for (int filas=0;  filas< ROW; filas++){
@@ -110,5 +180,8 @@ void Asientos(){
     }printf("\n");     
 }
 }
+printf("Presione Enter para volver al menu principal\n");
+getchar();
 return;
+
 }
